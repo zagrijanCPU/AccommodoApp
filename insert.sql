4efa1852c4bd4a1e7d597ca30c9b28c246b2c200
filipmiskovic <fm134913696@fer.hr>
2024-03-22 08:58:39 +0100

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,14 @@
+insert into korisnik (korisnickoIme, ime, prezime, email, lozinka, tipkorisnika)
+values('admin', NULL, NULL, 'admin@accommodoapp.com', 'pass', 'admin')
+
+insert into korisnik (korisnickoIme, ime, prezime, email, lozinka, tipkorisnika)
+values('user1', 'Iva', 'Ivić', 'ivaivic@gmail.com', 'pass', 'gost')
+
+insert into korisnik (korisnickoIme, ime, prezime, email, lozinka, tipkorisnika)
+values('user2', 'Marko', 'Markić', 'markomarkic@gmail.com', 'pass', 'gost')
+
+insert into korisnik (korisnickoIme, ime, prezime, email, lozinka, tipkorisnika)
+values('user3', 'Mate', 'Matić', 'matematic@gmail.com', 'pass', 'gost')
+
+insert into korisnik (korisnickoIme, ime, prezime, email, lozinka, tipkorisnika)
+values('user4', 'Fran', 'Franić', 'franfranic@gmail.com', 'pass', 'gost')
